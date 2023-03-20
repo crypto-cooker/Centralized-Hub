5b879a0a20ee444b4af9f1762eebb80d05cb5041
mateo-kova <mateo-kova0510@gmail.com>
2023-03-20 18:28:13 +0900

    fix issue

--- pages/set-new-password.tsx
+++ pages/set-new-password.tsx
@@ -2,6 +2,7 @@ import Button from "components/Button";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { resetPass } from "actions/forgot";
+import Input from "components/Input";
 export default function setNewPassword(
   props: {
     startLoading: Function;
@@ -15,6 +16,14 @@ export default function setNewPassword(
   const [email, setEmail] = useState<string>("");
   const [passcode, setPasscode] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [passLevelMsg, setPassLevelMsg] = useState<string>("");
+  const [passLevelStatus, setPassLevelStatus] = useState<string>("success");
+  const [passValidations, setPassValidations] = useState<boolean[]>([
+    false,
+    false,
+    false
+  ]);
+  const [validation, setValidation] = useState<boolean>(false);
 
   const router = useRouter();
   useEffect(() => {
@@ -22,10 +31,49 @@ export default function setNewPassword(
 
     const getEmail = router.query.email;
     const getPasscode = router.query.passwordCode;
+
     setEmail(getEmail?.toString());
     setPasscode(getPasscode?.toString());
-    setPassword("password");
   }, [router]);
+  const handlePassInputChange = (e) => {
+    const pass: string = e.target.value ?? "";
+    let validations = Object.assign(passValidations);
+    const regex = /[a-zA-Z0-9]/;
+    const spRegex = /[^a-zA-Z0-9]/g;
+    let count = 0;
+    validations[2] = regex.test(pass);
+    count += regex.test(pass) ? 1 : 0;
+    validations[1] = pass.length > 7;
+    count += pass.length > 7 ? 1 : 0;
+    count += spRegex.test(pass) ? 1 : 0;
+    count += pass.length > 11 ? 1 : 0;
+    validations[0] = count > 1;
+    switch (count) {
+      case 1:
+        setPassLevelStatus("error");
+        setPassLevelMsg("Too weak");
+        break;
+      case 2:
+        setPassLevelStatus("warning");
+        setPassLevelMsg("Better");
+        break;
+      case 3:
+        setPassLevelStatus("success");
+        setPassLevelMsg("Strong");
+        break;
+      case 4:
+        setPassLevelStatus("success");
+        setPassLevelMsg("Very String");
+        break;
+      default:
+        setPassLevelStatus(" ");
+        setPassLevelMsg("Too weak");
+    }
+    if (!pass) setPassLevelMsg("");
+
+    setPassValidations(validations);
+    setPassword(e.target.value);
+  };
   const resetPassword = async () => {
     const resetRes = await resetPass(email, password, passcode);
     console.log(resetRes);
@@ -47,6 +95,18 @@ export default function setNewPassword(
                   Click the button below to rest your password. If you did not
                   request a password rest, you can disregard this message.
                 </div>
+                {/* <div className="w-full mt-8">
+                  <Input
+                    className="border-2"
+                    value={password}
+                    placeholder="Reset Password"
+                    title="Reset Password"
+                    type="password"
+                    error={passLevelMsg}
+                    status={passLevelStatus}
+                    onChange={handlePassInputChange}
+                  />
+                </div> */}
                 {/* <div className="text-xl tracking-widest">
                   Click on the link in the email to complete your registeration
                 </div> */}
